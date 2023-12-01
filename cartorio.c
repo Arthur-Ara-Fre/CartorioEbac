e88a77bbdb1cf59bfd995597f16a3f5b588ec015
Arthur-Ara-Fre <152612993+Arthur-Ara-Fre@users.noreply.github.com>
2023-12-01 10:22:52 -0300

    Nomes com espaço, identação e buffer do teclado.
    o scanf() foi trocado pelo gets() para pegar nome com espaço como João Pedro, ou engenheiro chefe, além de consertar um problema do buffer do teclado onde digitar um nome com espaço acabava pulando uma parte do registro e consequentemente gravando a palavra depois do espaço no preenchimento seguinte. Foi usado fflsush(stdin) para consertar esse problema.

--- cartorio.c
+++ cartorio.c
@@ -1,31 +1,36 @@
-#include <stdio.h> //biblioteca de comunicação com o usuário
-#include <stdlib.h>//biblioteca de alocação de espaço em memória
-#include <locale.h>//biblioteca de alocações de texto por região
-#include <string.h>//biblioteca responsável por cuidar das strings
+#include <stdio.h> //biblioteca de comunicação com o usuário.
+#include <stdlib.h>//biblioteca de alocação de espaço em memória.
+#include <locale.h>//biblioteca de alocações de texto por região.
+#include <string.h>//biblioteca responsável por cuidar das strings.
 
-int registro()//função responsavel por cadastrar usuarios no sistema
+int registro()//função responsável por cadastrar usuários no sistema.
 {
-	//inicio das funções das variaveis/strings
+	//início das funções das variaveis/strings.
 	char arquivo[40];
 	char cpf[40];
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
-	int recadastro;
-	//Final das funções das variaveis/strings
+	int recadastro=0;
+	//Final das funções das variáveis/strings.
 	
-	printf("digite o CPF a ser cadastrado: ");//coletando informação do usuario
-	scanf("%s", cpf);//%s referente a string
+	printf("digite o CPF a ser cadastrado: ");//coletando informação do usuário.
+	scanf("%s", cpf);//%s referente a string.
+	fflush(stdin);
 	
-	//Quando um usuario ja existente é cadastrado, o antigo cadastro é substituido.
-	//Deixar o cliente ciente de que o cadastro que ele está fazendo já existe 
+	//Quando um usuário ja existente é cadastrado, o antigo cadastro é substituído sem o usuario saber.
+	//Alem de gastar mais tempo cadastrando um usuario que ja tinha sido casdastrado.
+	//Deixar o cliente ciente de que o cadastro que ele está fazendo já existe.
 	
-	FILE *file;//cria o arquivo
+	//começo do re-cadastro.
+	
+	FILE *file;//cria o arquivo.
 	file = fopen(cpf, "r");
 	if (file != NULL)
 	{
 		printf("Já existe um usuario com este cpf cadastrado. Deseja sobrecreve-lo?\n1-Sim\n2-Não\n\nopção:");
 		scanf("%d", &recadastro);
+		fflush(stdin);//Limpar o buffer do teclado.
 	}
 	fclose(file);
 	printf("\n");
@@ -33,23 +38,24 @@ int registro()//função responsavel por cadastrar usuarios no sistema
 	{
 		return 0;
 	}
-	//fim do recadastro
+	//fim do re-cadastro.
 	system("cls");
 	printf("CPF cadastrado:%s\n", cpf);
 	
-	strcpy (arquivo, cpf);//responsável por copiar os valores das strings
+	strcpy (arquivo, cpf);//responsável por copiar os valores das strings.
 	
-	file = fopen(arquivo, "w");//cria o arquivo. "w" significa escrever
+	file = fopen(arquivo, "w");//cria o arquivo. "w" significa escrever.
 	fprintf(file, "CPF:");
-	fprintf(file,cpf);//salva o valor da variavel
-	fclose(file);//fecha o arquivo
+	fprintf(file,cpf);//salva o valor da variavel.
+	fclose(file);//fecha o arquivo.
 	
 	file = fopen(arquivo, "a");//virgula
 	fprintf(file, "\n");
 	fclose(file);
 	
 	printf("digite o nome a ser cadastrado: ");
-	scanf("%s", nome);
+	gets(nome);//pegar strings com espaço, visto que o scanf não faz.
+	fflush(stdin);
 	
 	file = fopen(arquivo, "a");
 	fprintf(file, "Nome: ");
@@ -61,7 +67,8 @@ int registro()//função responsavel por cadastrar usuarios no sistema
 	fclose(file);
 	
 	printf("digite o sobrenome a ser cadastrado: ");
-	scanf("%s", sobrenome);
+	gets(sobrenome);
+	fflush(stdin);
 	
 	file = fopen(arquivo, "a");
 	fprintf(file, "Sobrenome: ");
@@ -73,7 +80,8 @@ int registro()//função responsavel por cadastrar usuarios no sistema
 	fclose(file);
 	
 	printf("digite o cargo a ser cadastrado: ");
-	scanf("%s", cargo);
+	gets(cargo);
+	fflush(stdin);
 	
 	file = fopen(arquivo, "a");
 	fprintf(file, "Cargo: ");
@@ -85,7 +93,7 @@ int registro()//função responsavel por cadastrar usuarios no sistema
 
 int consulta()
 {
-	setlocale (LC_ALL, "portuguese");//definindo a linguagem
+	setlocale (LC_ALL, "portuguese");//definindo o idioma.
 	
 	char cpf[40];
 	char conteudo[100];
@@ -150,28 +158,28 @@ int deletar()
 
 int main()
 {
-	int opcao = 0;//definindo as variáveis
+	int opcao = 0;//definindo as variáveis.
 	int laco = 1;
 	
-	setlocale (LC_ALL, "portuguese");//definindo a linguagem
+	setlocale (LC_ALL, "portuguese");//definindo a linguagem.
 	
 	for(laco = 1; laco = 1;)	
 	{
 		system("cls");
-		printf("### Cartório da EBAC ###\n\n");//inicio do menu
+		printf("### Cartório da EBAC ###\n\n");//inicio do menu.
 	
-		printf("Escolha a opção desejada do menu:\n\n");//Menu
+		printf("Escolha a opção desejada do menu:\n\n");//Menu.
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - consultar nomes\n");
 		printf("\t3 - Deletar nomes\n");
 		printf("\t4 - sair do programa\n\n");
-		printf("Opção: ");//fim do menu
+		printf("Opção: ");//fim do menu.
 	
-		scanf("%d", &opcao);//aramzenamento a escolha do usuário
+		scanf("%d", &opcao);//aramzenamento a escolha do usuário.
 		
-		system("cls");//limpeza da tela para o usuário
+		system("cls");//limpeza da tela para o usuário.
 		
-		switch (opcao)//inicio da seleção
+		switch (opcao)//início da seleção.
 		{
 			case 1:
 			registro();
@@ -193,6 +201,6 @@ int main()
 			printf("Essa opção não está disponivel!\n");
 			system("pause");
 			break;
-		}//fim da seleção
+		}//fim da seleção.
 	}
 }
